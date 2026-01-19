c5226eb25e0cd08fc3e63b59e91d78774bb019eb
jomard <jomrdali44@gmail.com>
2026-01-18 20:40:28 -0400

    connect to dataBase

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-
-app.listen(process.env.PORT, () => {
-  console.log("server is runing");
-});
+mongoose.connect(process.env.MONGO).then(
+  app.listen(process.env.PORT, () => {
+    console.log("server is runing");
+  }),
+);
