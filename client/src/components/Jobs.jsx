8a32d12acba92e368e576595b508bcae12028955
jomard <jomrdali44@gmail.com>
2026-01-19 18:24:49 -0400

    created a jobs page

--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -1,12 +1,36 @@
-import JobCard from "./JobCard"
+import JobCard from "./JobCard";
+import { useState, useEffect } from "react";
 function Jobs() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/jobs");
+        const fetchedJobs = await res.json();
+        if(!fetchedJobs){
+            console.log("there are no jobs ")
+            return <p className="text-red-100 flex align-center justify-center">There are no jobs to be displayed</p>
 
+        }
+        setJobs(fetchedJobs);
+      } catch (err) {
+        console.error("error when fetching data", err);
+      }
+    };
+    getData();
+  }, []);
   return (
     <div>
-        <JobCard company_name={"IBM"} position={"Backend"} status={"Applied"}/>
-      
+      {jobs.map((job) => (
+        <JobCard
+          key={job._id}
+          company_name={job.company_name}
+          position={job.position}
+          status={job.status}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Jobs
+export default Jobs;
