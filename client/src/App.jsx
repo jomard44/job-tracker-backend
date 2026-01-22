cc8ede8856bdbd020b49e7886473ead739d3b8a7
jomard <jomrdali44@gmail.com>
2026-01-21 20:50:56 -0400

    added navigation to client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
-import "./app.css"
-import NaveBar from "./components/NaveBar"
-import Jobs from "./components/Jobs"
-import AddJob from "./components/AddJob"
+import "./app.css";
+import NavBar from "./components/NaveBar";
+import Jobs from "./components/Jobs";
+import AddJob from "./components/AddJob";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <> 
-    <NaveBar/>
-    <Jobs/>
-    <AddJob/>
+    <>
+     
+
+      <BrowserRouter>
+       <NavBar />
+        <Routes>
+          <Route path="/" element={<Jobs />} />
+          <Route path="/add-job" element={<AddJob />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/NaveBar.jsx
+++ client/src/components/NaveBar.jsx
@@ -1,13 +1,13 @@
 import "../index.css";
-
+import { Link } from "react-router-dom";
 function NaveBar() {
   return (
     <nav className="bg-gray-600 shadow p-4 flex justify-between">
-      <h1 className="text-red-500 font-bold ">Job Tracker</h1>
+      <h1 className="text-red-500 font-bold "><Link to="/">Job Tracker</Link></h1>
 
-      <button className=" text-white  text-lg hover:text-blue-200">
-      Add a new job
-      </button>
+      <Link className=" text-white  text-lg hover:text-blue-200" to="/add-job">
+        Add a new job
+      </Link>
     </nav>
   );
 }
