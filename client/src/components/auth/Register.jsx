ae60ed7bdf0e9288f5a562b78458d99ce65028d9
jomard <jomrdali44@gmail.com>
2026-02-03 12:14:58 -0400

    updated class to className for react

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function Register() {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
@@ -24,39 +23,41 @@ function Register() {
       email: "",
       password: "",
     });
-    navigate("/")
+    navigate("/");
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      class="max-w-md mx-auto mt-20 p-6 bg-white rounded-xl shadow-lg space-y-4"
+      className="max-w-md mx-auto mt-20 p-6 bg-white rounded-xl shadow-lg space-y-4"
     >
-      <h2 class="text-2xl font-semibold text-center">Register</h2>
+      <h2 className="text-2xl font-semibold text-center">Register</h2>
 
-      <div class="flex flex-col">
-        <label class="text-sm font-medium mb-1">Email</label>
+      <div className="flex flex-col">
+        <label className="text-sm font-medium mb-1">Email</label>
         <input
           type="email"
-          class="border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          name="email"
+          className="border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="you@example.com"
           onChange={handleChange}
           value={user.email}
         />
       </div>
 
-      <div class="flex flex-col">
-        <label class="text-sm font-medium mb-1">Password</label>
+      <div className="flex flex-col">
+        <label className="text-sm font-medium mb-1">Password</label>
         <input
           type="password"
-          class="border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          name="password"
+          className="border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="••••••••"
           onChange={handleChange}
           value={user.password}
         />
       </div>
 
-      <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
+      <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
         Submit
       </button>
     </form>
