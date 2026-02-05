d8df1763d0902ab7b6890cc989aa878a038096f8
jomard <jomrdali44@gmail.com>
2026-02-04 23:03:54 -0400

    changed a link in navbar

--- client/src/components/NaveBar.jsx
+++ client/src/components/NaveBar.jsx
@@ -1,29 +1,30 @@
 import "../index.css";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Signout from "./auth/Signout";
 import { useAuth } from "../utils/AuthContext";
 
 function NaveBar() {
- const {isAuth} = useAuth()
-  if(isAuth === null){
-    return null
+  const { isAuth } = useAuth();
+  if (isAuth === null) {
+    return null;
   }
- 
+
   return (
     <nav className="bg-gray-600 shadow p-4 flex justify-between">
-      <h1 className="text-red-500 font-bold ">
-        <Link to="/">Job Tracker</Link>
-      </h1>
+      <h1 className="text-red-500 font-bold ">Job Tracker</h1>
       {isAuth && (
         <>
-        <Link
-          className=" text-white  text-lg hover:text-blue-200"
-          to="/add-job"
-        >
-          Add a new job
-        </Link>
-        <Signout/>
+          <h1 className="text-red-500 font-bold ">
+            <Link to="/">Job Tracker</Link>
+          </h1>
+          <Link
+            className=" text-white  text-lg hover:text-blue-200"
+            to="/add-job"
+          >
+            Add a new job
+          </Link>
+          <Signout />
         </>
       )}
     </nav>
