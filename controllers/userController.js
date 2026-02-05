9c6b6cd1bdefdbb90f81a505f7f0e30f776e1232
jomard <jomrdali44@gmail.com>
2026-02-04 22:48:52 -0400

    updated cookies for cross origin access

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,8 +33,8 @@ export const register = async (req, res) => {
     // });
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -73,8 +73,8 @@ export const login = async (req, res) => {
     // });
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
