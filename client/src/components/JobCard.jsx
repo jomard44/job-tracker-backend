7470ecdea98d9b382c0f6c9d49e0aaf35fc2a187
jomard <jomrdali44@gmail.com>
2026-02-15 15:28:30 -0400

    made the site responsive

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -2,44 +2,28 @@ import {Link} from "react-router-dom"
 function JobCard({id, company_name, position, status, description, link}) {
   
   return (
-    <div className="bg-white/60 border border-gray-300 rounded-lg p-4 shadow-md m-3 flex items-center gap-4">
-      <input
-        className="flex-1 p-2 border rounded-md bg-gray-50 text-gray-700 font-medium"
-        type="text"
-        value={company_name}
-        readOnly
-      />
-      {description && (<input
-        className="flex-1 p-2 border rounded-md bg-gray-50 text-gray-700 font-medium"
-        type="text"
-        value={description}
-        readOnly
-      />)}
-      
+       <div className="bg-white/60 border border-gray-300 rounded-lg p-4 shadow-md m-3 
+                    flex flex-col md:flex-row gap-3">
 
-      <input
-        className="displayFeild flex-1 p-2 border rounded-md bg-gray-50 text-gray-700" 
-        type="text"
-        value={position}
-        readOnly
-      />
+      <input className="w-full md:flex-1" value={company_name} readOnly />
 
-      <input
-        className="flex-1 p-2 border rounded-md bg-gray-50 text-gray-700"
-        type="text"
-        value={status}
-        readOnly
-      />
-      {link && (<input
-        className="displayFeild flex-1 p-2 border rounded-md bg-gray-50 text-gray-700 font-medium"
-        type="text"
-        value={link}
-        readOnly
-      />)}
-      
-    <Link className="p-4" to={`/edit/${id}`}>edit</Link>
-     <Link className="p-4" to={`/delete/${id}`}>delete</Link>
+      {description && (
+        <input className="w-full md:flex-1" value={description} readOnly />
+      )}
+
+      <input className="w-full md:flex-1" value={position} readOnly />
+      <input className="w-full md:flex-1" value={status} readOnly />
+
+      {link && (
+        <input className="w-full md:flex-1" value={link} readOnly />
+      )}
+
+      <div className="flex gap-3 justify-end md:justify-center">
+        <Link to={`/edit/${id}`}>edit</Link>
+        <Link to={`/delete/${id}`}>delete</Link>
+      </div>
     </div>
+
   );
 }
 
