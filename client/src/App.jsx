f9016086e97679d86ad443de7d17cfb1606b3d11
jomard <jomrdali44@gmail.com>
2026-02-15 23:51:12 -0400

    vercel source and destenations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Signin />} />
+          <Route path="*" element={<Signin />} />
           <Route path="/register" element={<Register />} />
           <Route element={<ProtectedRoutes />}>
             <Route path="/jobs" element={<Jobs />} />
