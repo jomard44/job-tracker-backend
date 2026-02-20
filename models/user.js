8649f336bec441a4dae37e3bfab71dc353e57136
jomard <jomrdali44@gmail.com>
2026-02-20 11:56:54 -0400

    add timestamp to the schemas

--- models/user.js
+++ models/user.js
@@ -10,6 +10,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-},{timeStamps: true});
+},{ timestamps: true });
 
 export default mongoose.model("User", userSchema);
